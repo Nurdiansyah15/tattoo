3c24ff3791df4545fec6753c1e028874fbbcb339
richal <richalfajril@gmail.com>
2023-04-14 17:12:17 +0700

    revisi terakhir

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,4 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
+  loop: "true",
   centerSlide: "true",
   fade: "true",
   grabCursor: "true",
@@ -14,17 +15,17 @@ var swiper = new Swiper(".mySwiper", {
     // when window width is >= 0 hp
     0: {
       slidesPerView: 1,
-      spaceBetween: 20,
+      spaceBetween: 10,
     },
     // when window width is >= 768 tablet
     768: {
       slidesPerView: 3,
-      spaceBetween: 40,
+      spaceBetween: 30,
     },
     // when window width is >= 992 dekstop
     992: {
       slidesPerView: 4,
-      spaceBetween: 30,
+      spaceBetween: 60,
     },
   },
 });
